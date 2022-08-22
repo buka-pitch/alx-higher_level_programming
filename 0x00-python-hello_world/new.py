676eac6ce804536f67bffafc376a9141dee445b5
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-08-22 22:22:35 +0300

    cut strings

--- 0x00-python-hello_world/new.py
+++ 0x00-python-hello_world/new.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
-Last = str[0:7]
-str = str[39:66] + str[107:111] + str[0:7]
-print(str)
+First = str[39:66]
+str = str[39:67] + str[107:111] + str[]
+print(f"{str[39:66]} {str[107:111]} {Last}")

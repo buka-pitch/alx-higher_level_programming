4e8564580854b618ecdce7f7281188750d4d94f6
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-08-22 22:15:18 +0300

    cut strings

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+First = str[39:66]
+Last = str[0:7]
+Middle = str[107:111]
+print(f"{First} {Middle} {Last}")

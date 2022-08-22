e0d7147ec20bad8adc63450e5bfdb73e13b59863
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-08-22 22:07:11 +0300

    cut strings

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+First = word[:2]
+Last = word[-2:]
+Middle = word[1:-1]
+print(f"First 3 letters: {First}")
+print(f"Last 2 letters: {Last}")
+print(f"Middle word: {Middle}")

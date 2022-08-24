620ead9ad947e2e5b63974b794c04f6c9fe83753
Buka <birukyonas331@gmail.com>
2022-08-24 18:25:49 +0300

    comb2

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(100):
+    if i < 10:
+        print("0{}".format(i),end=", ")
+    elif i == 99:
+        print("{}".format(i))
+    else:
+        print("{}".format(i),end=", ")
+        

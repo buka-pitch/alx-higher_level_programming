dc9f941f3b1c2b41bbb0f4a2c5d486df47e5da41
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-08-24 18:31:22 +0300

    comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i < 10:
-        print("0{}".format(i),end=", ")
-    elif i == 99:
-        print("{}".format(i))
-    else:
-        print("{}".format(i),end=", ")
-        
+for i in range(99):
+    print("{.02d}".format(i),end=", ")
+print(99)

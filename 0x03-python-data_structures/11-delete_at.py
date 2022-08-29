45a6beb08bbbeb86985b4492f14938a19b9a9bfd
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-08-30 00:01:22 +0300

    remove item

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     """function to delete (remove) specific item on the list using index """
-    if idx >= 0 or idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
         del my_list[idx]
         return my_listi
-    else:
-        return my_list

ac23caef78d5d896e35eaf788610130af6ee76e3
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-08-29 23:13:10 +0300

    remove any C or c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     """function to remove 'c' and 'C' charachters from any text"""
-    new_str = my_string.translate({ord(i): None for i in 'cC'})    
+    new_str = my_string.translate({ord(i): None for i in 'cC'})
     return new_str

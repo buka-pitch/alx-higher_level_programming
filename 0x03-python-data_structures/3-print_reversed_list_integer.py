6a6b00a0aa581de59d04618d3cd528ea4a83a7a4
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-08-29 22:49:49 +0300

    reverse a list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 def print_reversed_list_integer(my_list=[]):
     """function to reverse a list and print it each in a new line"""
     if my_list:
         my_list.reverse()
         for i in my_list:
             print("{:d}".format(i))
-

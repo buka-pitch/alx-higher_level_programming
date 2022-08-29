adbdc1e56de579a751f6ab9302a839f1f835f7a4
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-08-29 23:26:19 +0300

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    """function to take a matrix (nested list and print it)"""
     if matrix:
         for i in matrix:
             for j in i:
                 print("{}".format(j), end=' ')
-            print('\n', end='')
+            print('{}'.format('\n'), end='')

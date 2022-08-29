021737bea9ec2b02ccd7d1bc1c7ca1ec925e54a2
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-08-29 23:32:41 +0300

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,8 @@ def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in matrix:
             for j in i:
-                print("{}".format(j), end=' ')
+                if j == i[-1]:
+                    print("{:d}".format(j), end="")
+                else:
+                    print("{:d}".format(j), end=" ")
             print('{}'.format('\n'), end='')

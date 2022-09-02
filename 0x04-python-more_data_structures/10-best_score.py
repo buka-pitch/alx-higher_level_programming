dec248d6e1a930755cca21bfb6cf96267a1a8982
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-09-02 22:56:54 +0300

    max number from the dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     """
-        function to get the max number from the dictionary 
-        value and return the key
+    function to get the max number from the dictionary
+    value and return the key
     """
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
-    res = max(a_dictionary.items(),key=lambda n: n[1])[0]
+    res = max(a_dictionary.items(), key=lambda n: n[1])[0]
     return res

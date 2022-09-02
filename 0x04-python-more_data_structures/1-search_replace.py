0d8597ae02961add09fb04984b3ae72c619ad0cb
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-09-02 20:19:11 +0300

    replace a list item

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    """ function to replace specific item of a list 
-        by copying the list and finding the index of
-        the list then replacing the item
-        using the index with the new one
+    """ 
+    function to replace specific item of a list
+    finding the index of
+    the list then replacing the item
+    using the index with the new one
     """
-    new_list = my_list.copy()
-    i = my_list.index(search)
-    new_list[i] = replace
-    return new_list
-
-
-
+    return [replace if i == search for i in my_list]

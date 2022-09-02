f585f48266db1288cae02c9150715aceb2cda6e6
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-09-02 22:09:05 +0300

    simple delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     """delete a dict with a key"""
-    return a_dictionary.__delitem__(key)
+    if key in a_dictionary:
+        a_dictionary.__delitem__(key)
+    return a_dictionary

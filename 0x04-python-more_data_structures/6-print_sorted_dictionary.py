4061db15ec7514b6e1753b9b7f9efe1d4e31f34a
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-09-02 21:37:35 +0300

    sort dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,4 +5,4 @@ def print_sorted_dictionary(a_dictionary):
     function to sort a dict by order
     """
     for i in sorted(a_dictionary):
-        print('{} : {}'.format(i, a_dictionary[i]))
+        print('{}: {}'.format(i, a_dictionary[i]))

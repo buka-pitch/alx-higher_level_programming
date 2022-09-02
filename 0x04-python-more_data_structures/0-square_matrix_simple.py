d33a041f712431ea889a03f881f0b1a37c570956
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-09-02 20:22:26 +0300

    replace a list item

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix):
+def square_matrix_simple(matrix=[]):
     """ function to sqare the value of each item in the matrix"""
     return [[i**2 for i in row] for row in matrix]

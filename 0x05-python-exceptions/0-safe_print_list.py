f3d776c940a2c6a25b8cc60bef23ea6bb2d0b4a6
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-09-12 14:20:59 +0300

    print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
     """
     function to print x amount of the list in a safe way
     """
+    i = 0
     if x == 0:
-        return 0
+        return i
     try:
         for i in range(x):
             print(my_list[i], end="")

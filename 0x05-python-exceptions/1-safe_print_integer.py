3fed71b2ae2142c2ff38a0a96897443b2b2d5de8
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-09-12 14:54:48 +0300

    print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+from tkinter import W
+
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
-        return False
+    except ValueError:
+        print("{} is not an integer".format(value))
     return True

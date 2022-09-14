ef90b35fab22193e08679591292519b79dc01aa2
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-09-15 00:44:38 +0300

    square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,11 +6,20 @@ Square class with optional private arg size
 
 class Square:
     """create a square object
+    Args:
+        size
+    Returns:
+        ValueError when size is Negative value
+        TypeError when size is not integer
+
     """
 
-    def __init__(self, size=None):
+    def __init__(self, size=0):
         """Initialize the class square
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
 
-        if size not None:
-            self.size = size
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.size = size

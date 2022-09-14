66da65cf592d853bf879015d7ac489a118af0146
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-09-15 00:03:01 +0300

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """creates a Square class
+    """
+    def __init__(self):
+        """ Initialize the Square class.
+        """
+        self.__size = 0
+        

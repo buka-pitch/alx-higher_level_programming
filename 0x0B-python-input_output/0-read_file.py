fd3796c70925644eed817ea89a9b39f71d956d0e
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-09-28 19:47:24 +0300

    read files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,9 @@
 
 
 def read_file(filename=""):
+    """
+    function to read files from the filname in the argument
+    """
     if filename not None:
-        with open(filename, 'r') as f:
+        with open(filename, 'r', encoding='utf-8') as f:
             print(f.read())

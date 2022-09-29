64554ed9621963142fa5714e7d463b89d5c513e7
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-09-29 13:21:20 +0300

    write files

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
 """single function to write to a file"""
 
 
 def write_file(filename="", text=""):
     """function to write text to a specified file"""
     with open(filename, 'w', encoding='utf-8') as f:
-        f.write(text)
+        n = f.write(text)
+        return n

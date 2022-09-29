1a54390e3bf1ce1ab958f76cbd1d698628edf5c3
Buka Pitch <70853164+CreativeTechET@users.noreply.github.com>
2022-09-29 13:17:21 +0300

    write files

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,5 @@
 
 def write_file(filename="", text=""):
     """function to write text to a specified file"""
-    if filename and text is None:
-        raise ValueError("filename or text must be specified")
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(text)
